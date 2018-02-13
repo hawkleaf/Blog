0b7f564f47584d831d80ec581b842f398c8786d4
Joris Schelfhout <joris@pietersmit.nl>
2018-02-13 17:38:48 +0100

    Add role middleware

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Auth::routes();
 // Route::get('/profile/{user}', 'ProfileController@show')->name('profile.show');
 
 //Posts
-Route::group(['middleware' => 'can:manage posts'], function () {
+Route::group(['middleware' => 'role:Author'], function () {
     Route::get('/posts/create', 'PostController@create')->name('post.create');
     Route::post('/posts', 'PostController@store')->name('post.store');
     Route::delete('/posts/{post}', 'PostController@destroy')->name('post.destroy');
