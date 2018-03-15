8d8784ffc76756a5c7266297d33056d077eb3c8f
Joris Schelfhout <joris@pietersmit.nl>
2018-03-15 15:32:10 +0100

    Add edit endpoint for blog posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use Api\Post\Http\Requests\PostRequest;
+use App\Http\Requests\Post\UpdatePostRequest;
 use Carbon\Carbon;
 use Auth;
 
@@ -34,11 +35,23 @@ class PostController extends Controller
         return view('post.show', compact('post'));
     }
 
+    public function edit(Post $post)
+    {
+        return view('post.edit', compact('post'));
+    }
+
     public function destroy(Post $post)
     {
         $this->authorize('owner', $post);
 
         $post->delete();
+
+        return redirect()->route('post.index');
+    }
+
+    public function update(Post $post, UpdatePostRequest $request)
+    {
+        $post->update($request->validated());
 
         return redirect()->route('post.index');
     }
